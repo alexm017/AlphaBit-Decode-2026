6fe03d9243ef0e45f4de72b23cfd9292b6ce4ccd
alexm017 <pandalescuvictor@gmail.com>
2026-01-02 20:51:50 +0200

    added reset heading to gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/GyroscopeBHIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Skeletal_Structures/GyroscopeBHIMU.java
@@ -23,4 +23,8 @@ public class GyroscopeBHIMU {
     public double getHeading(){
         return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
+
+    public void resetHeading(){
+        imu.resetYaw();
+    }
 }
