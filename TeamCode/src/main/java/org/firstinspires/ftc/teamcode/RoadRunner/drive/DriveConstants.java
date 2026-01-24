7aa2ad8c195524d178e852d62e3c8265a4f7186f
alexm017 <pandalescuvictor@gmail.com>
2026-01-24 20:54:59 +0200

    increase autonomous speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
@@ -65,8 +65,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 35; //45
-    public static double MAX_ACCEL = 35; //42.5
+    public static double MAX_VEL = 40; //45
+    public static double MAX_ACCEL = 40; //42.5
     public static double MAX_ANG_VEL = Math.toRadians(160);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
