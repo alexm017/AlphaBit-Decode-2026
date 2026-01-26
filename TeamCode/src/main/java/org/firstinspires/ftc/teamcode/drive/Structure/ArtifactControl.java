e43f7fc8c76108b437d694b6a9e60b94ab67d9cf
alexm017 <pandalescuvictor@gmail.com>
2026-01-26 22:01:45 +0200

    compensated basket distance due to closer target

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Structure/ArtifactControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Structure/ArtifactControl.java
@@ -685,7 +685,7 @@ public class ArtifactControl {
     public double getTurretAngle(){
         double angleToCm;
         double anglePerInch = Math.abs(((max_TurretAngleAuto-min_TurretAngleAuto)/max_TurretAngleDistance));
-        angleToCm = (basketDistance-15.55) * anglePerInch;
+        angleToCm = (basketDistance-4.25) * anglePerInch;
 
                 if(angleToCm > 0.55){
             angleToCm = 0.55;
@@ -698,9 +698,9 @@ public class ArtifactControl {
         double powerPerInch = Math.abs((max_FlyWheelPower-min_FlyWheelPower)/max_FlyWheelDistance);
         double distance;
         if(!useCustomPos) {
-            distance = basketDistance-15.55;
+            distance = basketDistance-4.25;
         }else{
-            distance = getBasketDistance(custom_x_pos, custom_y_pos, redAlliance, true)-15.55;
+            distance = getBasketDistance(custom_x_pos, custom_y_pos, redAlliance, true)-4.25;
         }
 
         double finalPower = min_FlyWheelPower + (distance * powerPerInch);
@@ -789,7 +789,7 @@ public class ArtifactControl {
         distanceToBasket = getBasketDistance(x_pos,y_pos,redAlliance,true);
 
         double anglePerInch = Math.abs(((max_TurretAngleAuto-min_TurretAngleAuto)/max_TurretAngleDistance));
-        double angleToCm = (distanceToBasket-15.55) * anglePerInch;
+        double angleToCm = (distanceToBasket-4.25) * anglePerInch;
 
         if(angleToCm > 0.7){
             angleToCm = 0.7;
